fab3e63c61dca52bf139473895aac8327b454b0e
lucasgarcia763 <lucas.garcia763@live.com>
2018-11-03 20:35:48 -0300

    Render do html com ejs

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
-var express = require("express");
+var express = require('express');
 
 var app = express();
 
-app.get("/produtos", function(req,res){
-    res.send("<html><body><h1>Listagem de Produtos</h1></body></html>");
+app.set('view engine', 'ejs');
+
+app.get('/produtos', function(req,res){
+    res.render('produtos/lista');
 });
 
 app.listen(3000, function(){
-    console.log("servidor rodando");
+    console.log('Servidor rodando!');
 
 });
 
-app.get("/", function(req,res){
-    res.send("<h1>Home</h1>");
+app.get('/', function(req,res){
+    res.send('<h1>Home</h1>');
 });
 
 
